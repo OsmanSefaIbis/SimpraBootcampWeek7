3f973a4047bb3d6e7146968dce017788f602ba08
Sefa IBIS <osmansefaibis@gmail.com>
2023-01-18 20:08:55 +0300

    Foldering added, finalized the project code

--- week7CoreData/Managers/InternetManager.swift
+++ week7CoreData/Managers/InternetManager.swift
@@ -46,7 +46,8 @@ class InternetManager{
                 let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
                 let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
                 let ret = (isReachable && !needsConnection)
-
-                return ret
+                // to CoreData make the flag false
+                return false
+                //return ret
     }
 }
